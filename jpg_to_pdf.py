d02d6d2d0a744d6bf7e101ca3215610ea379ce7b
Suraj <suraj3621zi@gamil.com>
2026-08-29 16:57:55 +0530

    image.jpg -> pdf

--- jpg_to_pdf.py
+++ jpg_to_pdf.py
@@ -3,10 +3,23 @@ import fitz
 # Create Document Object 
 pdf = fitz.open()
 
-# Create Empty page[0]
-page = pdf.new_page()
+img = fitz.Pixmap("photo.jpg")
+
+rect = fitz.Rect(img.irect)
+
+# Create Empty page[0] with img's width $  height
+page = pdf.new_page(
+    width=rect.width,
+    height=rect.height
+)
+
+# Now insert image
+page.insert_image(
+    rect=rect,
+    filename="photo.jpg"
+)
 
 pdf.save("new.pdf")
 pdf.close()
 
-print(page)
+print("You're photo --> pdf is ready !!!")
